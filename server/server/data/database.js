99dcc8ebdd4a7042ff503e31d4aed992cc487e5a
Darshankumar chatrawat (Jain) <101007397+darshan15062002@users.noreply.github.com>
2026-02-07 11:44:57 +0530

    Change MongoDB connection URI in database.js
    Updated MongoDB connection string to use a direct URI.

--- server/server/data/database.js
+++ server/server/data/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
-        const { connection } = await mongoose.connect(process.env.MONGO_URI);
+        const { connection } = await mongoose.connect("mongodb+srv://darshanjain15062002:vBKhKJidKAiPI0v5@cluster0.sltrs2g.mongodb.net/new");
 
         console.log(`Server connected to database ${connection.host}`);
     } catch (error) {
